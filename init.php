addf08f4d036e3c1b085d7d1cd631c17cec52833
Berdnikov Alexey <berdnikov_ay@taximaxim.ru>
2017-03-22 13:23:04 +0500

    fixed

--- init.php
+++ init.php
@@ -2,4 +2,6 @@
 \Larakit\StaticFiles\Manager::package('larakit/sf-jquery-nestable')
     ->setSourceDir('public')
     ->usePackage('larakit/sf-jquery')
-    ->jsPackage('jquery.nestable.js');
+    ->jsPackage('jquery.nestable.js')
+    ->cssPackage('jquery.nestable.css')
+;
